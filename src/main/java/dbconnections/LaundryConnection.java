22d4e84875288b8c6c40702c92ee8d9b662c67b5
Kingsley Henshaw <ayehenz29@gmail.com>
2022-08-08 13:48:31 +0100

    First push for work

---
+++ src/main/java/dbconnections/LaundryConnection.java
@@ -0,0 +1,22 @@
+package dbconnections;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class LaundryConnection {
+    Connection connection;
+
+    public boolean dbconnection(){
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/laundry", "root","");
+            return true;
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+    public Connection getConnections() {
+        return connection;
+}
+}
