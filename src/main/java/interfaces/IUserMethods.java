5b254847ee8b355bcefbac03c9242418559ed77f
Kingsley Henshaw <ayehenz29@gmail.com>
2022-08-08 15:05:17 +0100

    Created Interfaces For Users

---
+++ src/main/java/interfaces/IUserMethods.java
@@ -0,0 +1,10 @@
+package interfaces;
+
+import model.Users;
+
+public interface IUserMethods {
+    public boolean dropClothes();
+    public String checkClothes();
+    public String addClothes();
+    public String pickUpClothes();
+}
